1190dc9c42b5d2853cc336a5f1eb795c703cb47f
mshomerai03 <seo_kor@hotmail.com>
2026-04-18 11:18:42 +0800

    lingma

---
+++ code_war/7k/counting_sheep.py
@@ -0,0 +1,10 @@
+# [True,  True,  True,  False,
+#   True,  True,  True,  True ,
+#   True,  False, True,  False,
+#   True,  False, False, True ,
+#   True,  True,  True,  True ,
+#   False, False, True,  True]
+# count = 17
+
+def count_sheeps(sheep):
+    return sheep.count(True)
